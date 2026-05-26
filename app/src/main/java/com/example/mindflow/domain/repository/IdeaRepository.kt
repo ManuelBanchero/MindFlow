42661fdf7d66f1aa5f8dd6c8652c2b93cfc58182
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-26 09:09:36 -0300

    interface of user repository created

---
+++ app/src/main/java/com/example/mindflow/domain/repository/IdeaRepository.kt
@@ -0,0 +1,4 @@
+package com.example.mindflow.domain.repository
+
+interface IdeaRepository {
+}
---
+++ app/src/main/java/com/example/mindflow/domain/repository/UserRepository.kt
@@ -0,0 +1,23 @@
+package com.example.mindflow.domain.repository
+
+import com.example.mindflow.domain.model.User
+
+interface UserRepository {
+    suspend fun createUser(
+        firstName: String,
+        lastName: String,
+        mail: String,
+        password: String
+    ): Result<Unit>
+
+    suspend fun logIn(
+        mail: String,
+        password: String
+    ): Result<User>
+
+    suspend fun getActiveSession(): User?
+
+    suspend fun logOut(userId: Int): Result<Unit>
+
+    suspend fun subscribeToPlan(userId: Int): Result<Unit>
+}
