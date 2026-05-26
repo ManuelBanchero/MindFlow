610f72bb14e63ef36c96c427b08bc67ba8d6733e
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-26 11:36:18 -0300

    Result data type return changed

--- app/src/main/java/com/example/mindflow/data/remote/datasource/UserRemoteDataSource.kt
+++ app/src/main/java/com/example/mindflow/data/remote/datasource/UserRemoteDataSource.kt
@@ -5,9 +5,9 @@ import com.example.mindflow.data.remote.dto.SignInRequest
 import com.example.mindflow.data.remote.dto.UserDTO
 
 interface UserRemoteDataSource {
-    suspend fun logIn(loginRequest: LoginRequest): Result<UserDTO>
+    suspend fun logIn(loginRequest: LoginRequest): UserDTO
 
-    suspend fun signIn(signInRequest: SignInRequest): Result<UserDTO>
+    suspend fun signIn(signInRequest: SignInRequest): UserDTO
 
-    suspend fun subscribeToPlan(userId: Int): Result<Unit>
+    suspend fun subscribeToPlan(userId: Int)
 }
