6b3758490f90f8bafbc68aa1bd263bb34548696c
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-27 21:04:05 -0300

    use case: Get Ideas w/Flow

---
+++ app/src/main/java/com/example/mindflow/domain/usecase/GetIdeasUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.mindflow.domain.usecase
+
+import com.example.mindflow.domain.model.Idea
+import com.example.mindflow.domain.repository.IdeaRepository
+import com.example.mindflow.domain.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+
+class GetIdeasUseCase(
+    private val userRepository: UserRepository,
+    private val ideaRepository: IdeaRepository
+) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<List<Idea>> {
+        return flow {
+            val user = userRepository.getActiveSession()
+            emit(user)
+        }.flatMapLatest { user ->
+            if (user != null) {
+                ideaRepository.getIdeasFlow(user.id)
+            } else {
+                flow { emit(emptyList()) }
+            }
+        }
+    }
+}
