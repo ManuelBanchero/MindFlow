80809ff28d066fbcc3ddb7f278141ac15905a3c0
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-28 17:23:40 -0300

    navigation

--- app/src/main/java/com/example/mindflow/MainActivity.kt
+++ app/src/main/java/com/example/mindflow/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.mindflow.ui.presentation.idealist.IdeaListScreen
 import com.example.mindflow.ui.presentation.login.LoginScreen
 import com.example.mindflow.ui.theme.MindFlowTheme
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.hilt.navigation.compose.hiltViewModel // Importante
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,14 +23,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindFlowTheme {
-                LoginScreen(
-                    onNavigateToMainPage = {
-                        println("Navegación exitosa")
-                    },
-                    viewModel = hiltViewModel()
-                )
+                MindFlowAppNavigation()
             }
         }
     }
 }
 
+@Composable
+fun MindFlowAppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        // Pantalla de Login
+        composable("login") {
+            LoginScreen(
+                onLoginSuccess = {
+                    navController.navigate("idea_list") {
+                        popUpTo("login") { inclusive = true }
+                    }
+                },
+                viewModel = hiltViewModel()
+            )
+        }
+
+        // Pantalla de Lista de Ideas
+        composable("idea_list") {
+            IdeaListScreen(
+                onNavigateToIdeaDetail = { ideaId ->
+                    navController.navigate("idea_detail/$ideaId")
+                },
+                viewModel = hiltViewModel()
+            )
+        }
+
+        // Pantalla de Detalle (Placeholder por ahora)
+        composable(
+            route = "idea_detail/{ideaId}",
+            arguments = listOf(navArgument("ideaId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val ideaId = backStackEntry.arguments?.getInt("ideaId") ?: 0
+            // Aquí iría tu IdeaDetailScreen(ideaId)
+        }
+    }
+}
