a7a003ee9a56e3837e659f455b9ca0591b9278ac
ManuelBanchero <manuelbanchero04@gmail.com>
2026-05-28 21:05:11 -0300

    screen: create idea

---
+++ app/src/main/java/com/example/mindflow/ui/presentation/createidea/CreateIdeaScreen.kt
@@ -0,0 +1,26 @@
+package com.example.mindflow.ui.presentation.createidea
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun CreateIdeaScreen(
+    onCreateIdeaSuccess: (Int) -> Unit,
+    viewModel: CreateIdeaViewModel
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(uiState.createdIdeaId) {
+        val id: Int? = uiState.createdIdeaId
+        if (id != null) {
+            onCreateIdeaSuccess(id)
+        }
+    }
+
+    CreateIdeaContent(
+        uiState,
+        onEvent = { event -> viewModel.onEvent(event) }
+    )
+}
