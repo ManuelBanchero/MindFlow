7d9f5766d8ddfc3cad030c5e44635ce1c437d944
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-12 15:12:48 -0300

    dependency injection updated

--- app/src/main/java/com/example/mindflow/data/di/DataModule.kt
+++ app/src/main/java/com/example/mindflow/data/di/DataModule.kt
@@ -1,6 +1,10 @@
 package com.example.mindflow.data.di
 
+import com.example.mindflow.data.local.file.FileManager
+import com.example.mindflow.data.local.file.impl.InternalFileManager
+import com.example.mindflow.data.local.hardware.AudioDataSource
 import com.example.mindflow.data.local.hardware.SpeechToTextDataSource
+import com.example.mindflow.data.local.hardware.impl.AndroidAudioDataSource
 import com.example.mindflow.data.local.hardware.impl.MockSpeechToTextDataSource
 import com.example.mindflow.data.remote.datasource.IdeaProcessorDataSource
 import com.example.mindflow.data.remote.datasource.IdeaRemoteDataSource
@@ -10,8 +14,10 @@ import com.example.mindflow.data.remote.datasource.impl.MockIdeaRemoteDataSource
 import com.example.mindflow.data.remote.datasource.impl.MockUserRemoteDataSource
 import com.example.mindflow.data.repository.IdeaRepositoryImpl
 import com.example.mindflow.data.repository.UserRepositoryImpl
+import com.example.mindflow.data.service.AudioRecorderImpl
 import com.example.mindflow.domain.repository.IdeaRepository
 import com.example.mindflow.domain.repository.UserRepository
+import com.example.mindflow.domain.service.AudioRecorder
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -57,4 +63,22 @@ abstract class DataModule {
     abstract fun bindSpeechToTextDataSource(
         mockSpeechToTextDataSource: MockSpeechToTextDataSource
     ): SpeechToTextDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindFileManager(
+        internalFileManager: InternalFileManager
+    ): FileManager
+
+    @Binds
+    @Singleton
+    abstract fun bindAudioDataSource(
+        androidAudioDataSource: AndroidAudioDataSource
+    ): AudioDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindAudioRecorder(
+        audioRecorderImpl: AudioRecorderImpl
+    ): AudioRecorder
 }
