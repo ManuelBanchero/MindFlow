88b8818b505f342c1a6c13883f97e8fe441f46ae
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-23 21:43:13 -0300

    short way to do it

--- app/src/main/java/com/example/mindflow/data/remote/datasource/impl/MockIdeaRemoteDataSource.kt
+++ app/src/main/java/com/example/mindflow/data/remote/datasource/impl/MockIdeaRemoteDataSource.kt
@@ -2,37 +2,14 @@ package com.example.mindflow.data.remote.datasource.impl
 
 import com.example.mindflow.data.remote.datasource.IdeaRemoteDataSource
 import com.example.mindflow.data.remote.dto.IdeaDTO
-import com.example.mindflow.data.remote.dto.ProcessedIdeaDraftDTO
-import com.example.mindflow.data.remote.dto.QuestionDTO
-import java.time.Instant
 import javax.inject.Inject
 
 class MockIdeaRemoteDataSource @Inject constructor(): IdeaRemoteDataSource {
     override suspend fun saveIdea(
-        processedIdeaDraftDTO: ProcessedIdeaDraftDTO,
-        audioTranscribed: String,
+        ideaDTO: IdeaDTO,
         userId: Int
     ): IdeaDTO {
-        return IdeaDTO(
-            id = 1,
-            userId = 1,
-            title = processedIdeaDraftDTO.title,
-            createdAt = Instant.now().toEpochMilli(),
-            updatedAt = Instant.now().toEpochMilli(),
-            category = processedIdeaDraftDTO.category,
-            textsAudioHistory = listOf(audioTranscribed),
-            summarizeContent = processedIdeaDraftDTO.summarizeContent,
-            structuredIdea = processedIdeaDraftDTO.structuredIdea,
-            questions = processedIdeaDraftDTO.questions.mapIndexed {
-                i, it -> QuestionDTO(
-                    id = i + 1,
-                    ideaId = 1,
-                    category = it.category,
-                    questionText = it.questionText,
-                    description = it.description
-                )
-            }
-        )
+        return ideaDTO.copy(userId = userId)
     }
 
     override suspend fun updateIdea(ideaDTO: IdeaDTO) {
