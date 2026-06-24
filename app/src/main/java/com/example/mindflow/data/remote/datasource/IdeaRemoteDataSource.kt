f7e72c44611a8f77d78a81f4792460af71fea983
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-23 21:48:13 -0300

    processedIdeaDto to ideaDto

--- app/src/main/java/com/example/mindflow/data/remote/datasource/IdeaRemoteDataSource.kt
+++ app/src/main/java/com/example/mindflow/data/remote/datasource/IdeaRemoteDataSource.kt
@@ -1,12 +1,10 @@
 package com.example.mindflow.data.remote.datasource
 
 import com.example.mindflow.data.remote.dto.IdeaDTO
-import com.example.mindflow.data.remote.dto.ProcessedIdeaDraftDTO
 
 interface IdeaRemoteDataSource {
     suspend fun saveIdea(
-        processedIdeaDraftDTO: ProcessedIdeaDraftDTO,
-        audioTranscribed: String,
+        ideaDTO: IdeaDTO,
         userId: Int
     ): IdeaDTO
     suspend fun updateIdea(ideaDTO: IdeaDTO)
