a2292f498e2674fb28095cefa30fd3033cdb589b
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-24 11:16:29 -0300

    updateIdea added

--- app/src/main/java/com/example/mindflow/data/remote/api/MindFlowApiService.kt
+++ app/src/main/java/com/example/mindflow/data/remote/api/MindFlowApiService.kt
@@ -2,9 +2,12 @@ package com.example.mindflow.data.remote.api
 
 import com.example.mindflow.data.remote.dto.IdeaDTO
 import okhttp3.MultipartBody
+import okhttp3.Request
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
 
@@ -20,4 +23,10 @@ interface MindFlowApiService {
     suspend fun deleteIdea(
         @Path("id") id: Int
     )
+
+    @PUT("ideas/{id}")
+    suspend fun updateIdea(
+        @Path("id") id: Int,
+        @Body request: IdeaDTO
+    ): IdeaDTO
 }
