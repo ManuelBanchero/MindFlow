edac04517ec3f702fb698c65743989e4c3551085
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-23 21:35:02 -0300

    light and dark color app added

--- app/src/main/java/com/example/mindflow/ui/theme/Color.kt
+++ app/src/main/java/com/example/mindflow/ui/theme/Color.kt
@@ -2,10 +2,46 @@ package com.example.mindflow.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Brand palette
+val MindFlowPurple = Color(0xFF9359FF)
+val MindFlowPurpleDeep = Color(0xFF6200FF)
+val MindFlowPurpleSoft = Color(0xFFE9DDFF)
+val MindFlowPurpleInk = Color(0xFF4B3A86)
+val MindFlowPurpleMuted = Color(0xFF5A4983)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light palette
+val PrimaryLight = Color(0xFF7B42F6)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val BackgroundLight = Color(0xFFFFFBFF)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceContainerLight = Color(0xFFF7F2FF)
+val OnSurfaceLight = Color(0xFF201A2E)
+val SubtitleLight = Color(0xFF6E667A)
+val LabelLight = Color(0xFF5D4A87)
+val InputLight = Color(0xFFFFFFFF)
+val PlaceholderLight = Color(0xFF958CA3)
+val ButtonLight = Color(0xFFEADDFF)
+val OnButtonLight = Color(0xFF32135F)
+val BorderLight = Color(0xFFE2DAEF)
+val ErrorContainerLight = Color(0xFFFFEDEC)
+val OnErrorContainerLight = Color(0xFF8A1F16)
+
+// Dark palette
+val PrimaryDark = Color(0xFFD8C2FF)
+val OnPrimaryDark = Color(0xFF2B1058)
+val BackgroundDark = Color(0xFF0E0B14)
+val SurfaceDark = Color(0xFF17131F)
+val SurfaceContainerDark = Color(0xFF211A2D)
+val OnSurfaceDark = Color(0xFFF7F0FF)
+val SubtitleDark = Color(0xFFD2C8DD)
+val LabelDark = Color(0xFFE8DAFA)
+val InputDark = Color(0xFF211B2A)
+val PlaceholderDark = Color(0xFFC0B4CD)
+val ButtonDark = Color(0xFFD8C2FF)
+val OnButtonDark = Color(0xFF2B1058)
+val BorderDark = Color(0xFF574D64)
+val ErrorContainerDark = Color(0xFF3B1512)
+val OnErrorContainerDark = Color(0xFFFFDAD6)
+
+// Shared accents
+val GradientCenter = MindFlowPurpleDeep
