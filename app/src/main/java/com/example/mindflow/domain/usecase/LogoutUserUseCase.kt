a66c0892e041c8c3e789b300f63fbb0b112236b8
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-25 16:25:20 -0300

    if user logs out -> delete all local ideas

--- app/src/main/java/com/example/mindflow/domain/usecase/LogoutUserUseCase.kt
+++ app/src/main/java/com/example/mindflow/domain/usecase/LogoutUserUseCase.kt
@@ -1,12 +1,17 @@
 package com.example.mindflow.domain.usecase
 
+import com.example.mindflow.domain.repository.IdeaRepository
 import com.example.mindflow.domain.repository.UserRepository
 import javax.inject.Inject
 
 class LogoutUserUseCase @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val ideaRepository: IdeaRepository
 ) {
     suspend operator fun invoke(): Result<Unit> {
+        ideaRepository.deleteAllLocalIdeas()
+            .onFailure { return Result.failure(it) }
+
         return userRepository.logOut()
     }
 }
