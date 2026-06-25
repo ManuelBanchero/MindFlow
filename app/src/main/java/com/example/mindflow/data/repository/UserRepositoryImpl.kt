91adb75d46e98e5afb568add889fe1cd04d7431a
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-25 16:28:35 -0300

    single responsibility principle applied

--- app/src/main/java/com/example/mindflow/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/mindflow/data/repository/UserRepositoryImpl.kt
@@ -2,10 +2,7 @@ package com.example.mindflow.data.repository
 
 import android.database.sqlite.SQLiteException
 import retrofit2.HttpException
-import androidx.room.withTransaction
 import com.example.mindflow.data.local.room.dao.UserDAO
-import com.example.mindflow.data.local.room.dao.IdeaDAO
-import com.example.mindflow.data.local.room.database.AppDatabase
 import com.example.mindflow.data.local.preferences.SessionPreferencesDataSource
 import com.example.mindflow.data.mapper.toDomain
 import com.example.mindflow.data.mapper.toEntity
@@ -22,9 +19,7 @@ import java.io.IOException
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val database: AppDatabase,
     private val userDao: UserDAO,
-    private val ideaDao: IdeaDAO,
     private val userRemoteDataSource: UserRemoteDataSource,
     private val sessionPreferencesDataSource: SessionPreferencesDataSource
 ): UserRepository {
@@ -100,10 +95,7 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun logOut(): Result<Unit> {
         return try {
-            database.withTransaction {
-                userDao.deleteUser()
-                ideaDao.deleteAllIdeas()
-            }
+            userDao.deleteUser()
             sessionPreferencesDataSource.setSessionActive(false)
             Result.success(Unit)
         } catch (e: SQLiteException) {
