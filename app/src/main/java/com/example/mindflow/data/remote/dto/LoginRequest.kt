89a0333c79d5e196f26f966950007e7f090ed002
ManuelBanchero <manuelbanchero04@gmail.com>
2026-06-25 12:46:11 -0300

    now is a valid DTO

--- app/src/main/java/com/example/mindflow/data/remote/dto/LoginRequest.kt
+++ app/src/main/java/com/example/mindflow/data/remote/dto/LoginRequest.kt
@@ -1,6 +1,12 @@
 package com.example.mindflow.data.remote.dto
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginRequest(
+    @SerialName("mail")
     val mail: String,
+    @SerialName("password")
     val password: String
 )
